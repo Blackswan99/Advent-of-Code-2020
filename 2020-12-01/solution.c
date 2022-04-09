d6dc02cb6d9370907d8d9f7edc93448c81809f36
Blackswan99 <37673853+Blackswan99@users.noreply.github.com>
2022-04-09 14:13:26 +0200

    Update solution.c
    First try

--- 2020-12-01/solution.c
+++ 2020-12-01/solution.c
@@ -1,1 +1,40 @@
-#
+// solution 2020/12/01
+#include <stdio.h>
+
+#define ARRAYSIZE 200
+
+int main(void)
+{
+    int numbers[ARRAYSIZE];
+    int i = 0, sum = 0, counter = 1, allcounter = 0;
+    FILE *file;
+
+    if ((file = fopen("numbers.txt", "r")))
+    {
+        while (fscanf(file, "%d", &numbers[i]) != EOF)
+        {
+            i++;
+        }
+        fclose(file);
+
+        while (counter < ARRAYSIZE)
+        {
+            numbers[i] = '\0';
+            for (i = counter - 1; numbers[i] != '\0'; i++)
+            {
+                if (i + 1 < ARRAYSIZE)
+                {
+                    sum = numbers[i] + numbers[i + 1];
+                    if (sum == 2020)
+                    {
+                        printf("num(i): %d, num(+1): %d ==> Sum: %d\n", numbers[i], numbers[i + 1], sum);
+                    }
+                }
+                allcounter++;
+            }
+            counter++;
+        }
+        printf("allcounter: %d\n", allcounter);
+    }
+    return 0;
+}
