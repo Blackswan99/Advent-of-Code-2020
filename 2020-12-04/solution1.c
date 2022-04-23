88d5f248b543d8b8f159a60981fdbe71ecc33985
Blackswan99 <37673853+Blackswan99@users.noreply.github.com>
2022-04-23 10:02:02 +0200

    Update solution1.c

--- 2020-12-04/solution1.c
+++ 2020-12-04/solution1.c
@@ -1,1 +1,22 @@
-#
+// solution 2020/12/04
+#include <stdio.h>
+
+int main(void)
+{
+
+    FILE *fp = fopen("passports.txt", "r");
+    int line_size = 128;
+    char line[128] = {'\0'};
+
+    while (1)
+    {
+        fgets(line, line_size, fp);
+        if (feof(fp))
+        {
+            break;
+        }
+
+        printf("%s", line);
+    }
+    return 0;
+}
